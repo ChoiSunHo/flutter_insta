f7ca5f25bee0866576b8917fbe1c3ad641ccf6ff
choeseonho <chltjsghz@naver.com>
2022-10-11 22:21:46 +0900

    second update

--- lib/src/init_bindings.dart
+++ lib/src/init_bindings.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 class InitBinding extends Bindings{
   @override
   void dependencies() {
-    Get.put(BottomNavController(),permanent:true);
+    Get.put(BottomNavController(),permanent:true);//
   }
 
 }
